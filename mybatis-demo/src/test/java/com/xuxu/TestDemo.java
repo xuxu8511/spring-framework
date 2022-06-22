085a11a7aa6fe5903143244b349765b895ea7530
xx <xuxu8511@163.com>
2022-06-22 13:47:37 +0800

    test demo

---
+++ mybatis-demo/src/test/java/com/xuxu/TestDemo.java
@@ -0,0 +1,15 @@
+package com.xuxu;
+
+import org.junit.Test;
+
+/**
+ * @author xuxu
+ * @date 2022/6/22
+ */
+public class TestDemo {
+
+	@Test
+	public void test() {
+		System.out.println("sss");
+	}
+}
