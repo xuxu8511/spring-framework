7e655570f83c51624701572144c17e35e89007d4
xx <xuxu8511@163.com>
2022-06-22 13:52:52 +0800

    test demo

--- mybatis-demo/src/main/java/com/xuxu/AppMain.java
+++ mybatis-demo/src/main/java/com/xuxu/AppMain.java
@@ -18,6 +18,9 @@ public class AppMain {
 		System.out.println(context.getBean("&xuXuFactoryBean") instanceof XuxuMapper);
 		//System.out.println("lubanFactoryBean: " + context.getBean(XuxuMapper.class));
 
+		System.out.println("this is test demo");
+
+		System.out.println("this is test demo333");
 		System.out.println("this is test demo222");
 	}
 }
